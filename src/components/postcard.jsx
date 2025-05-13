3c4ba9c076ea5700e7b4d7df82bce09e3bb700aa
vaisakh pb <vaisakhpb2005@gmail.com>
2025-05-13 11:48:50 +0530

    pushed

--- src/components/postcard.jsx
+++ src/components/postcard.jsx
@@ -1,14 +1,31 @@
-<div>
-      <h1>Digital Library</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        {books.map((book) => (
-          <BookCard
-            key={book.id}
-            image={book.image}
-            name={book.name}
-            genre={book.genre}
-            author={book.author}
-          />
-        ))}
+import React from 'react';
+import LikeButton from './likebutton';
+
+const PostCard = ({ post }) => {
+  return (
+    <div
+      style={{
+        border: '1px solid #ccc',
+        padding: '15px',
+        borderRadius: '10px',
+        marginBottom: '20px',
+        maxWidth: '400px',
+      }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+          src={post.profileImage}
+          alt="profile"
+          style={{ width: '50px', height: '50px', borderRadius: '50%' }}
+        />
+        <h3 style={{ marginLeft: '15px' }}>{post.username}</h3>
+      </div>
+      <div style={{ marginTop: '10px' }}>
+        <p>{post.content}</p>
+        <LikeButton initialLiked={post.isLiked} />
       </div>
     </div>
+  );
+};
+
+export default PostCard;
